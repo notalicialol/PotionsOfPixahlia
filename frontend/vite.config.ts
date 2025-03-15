dfaf4d8214a48e9a9b2d7ad182d043b8b5254b96
notalicialol <oiiinkoinkoink@gmail.com>
2025-03-15 17:40:56 -0400

    frontend basic setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv";
 
-// https://vite.dev/config/
+dotenv.config({ path: "../.env" });
+
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@components": "/src/components",
+			"@pages": "/src/pages",
+			"@routes": "/src/routes",
+			"@stores": "/src/stores",
+			"@styles": "/src/styles",
+			"@types": "/src/types",
+			"@utils": "/src/utils"
+		}
+	},
+	server: {
+		port: Number(process.env.VITE_FRONTEND_PORT),
+		proxy: {
+			"/api": {
+				target: `http://localhost:${process.env.VITE_BACKEND_PORT}`,
+				changeOrigin: true
+			}
+		}
+	}
+});
